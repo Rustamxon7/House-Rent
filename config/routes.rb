03e51b7441b5be881258e1bc8802595862b4cf6b
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-02-25 18:54:11 +0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/'
   mount Rswag::Api::Engine => '/api-docs'
   devise_for :users, defaults: { format: :json }, path: 'users',
-                     path_names: { sign_in: 'login', sign_out: 'logout', registration: 'signup' }
+                     path_names: { sign_in: 'signin', sign_out: 'signout', registration: 'signup' }
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
